a31ca34e52cb5427a54cff6cd62b6d898c8406e4
Ulla <gain@uef.fi>
2020-11-12 10:26:04 +0100

    students alcohol consumption data

---
+++ data/create_alc.R
@@ -0,0 +1,5 @@
+#Gain Ulla 12.11.2020  from data source two .csv files :
+# https://archive.ics.uci.edu/ml/machine-learning-databases/00320/student.zip  
+
+# web address for math class data
+url_math <- paste(url, "student-mat.csv", sep = "/"
